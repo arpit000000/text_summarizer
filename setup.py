93e4def5bc74d6a590dc1e674f8f1b29e81701bb
Arpit Jadon <tarpit324@gmail.com>
2025-03-30 10:42:34 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+with open("README.md", "r",encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text_summarizer"
+AUTHOR_USER_NAME = "arpit000000"
+SRC_REPO = "textSummarizer"
