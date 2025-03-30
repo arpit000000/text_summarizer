4f1f2310f698c448029171747eb8d13bec0944d3
Arpit Jadon <tarpit324@gmail.com>
2025-03-30 11:12:43 +0530

    requirements and installation done

--- setup.py
+++ setup.py
@@ -8,3 +8,21 @@ __version__ = "0.0.0"
 REPO_NAME = "text_summarizer"
 AUTHOR_USER_NAME = "arpit000000"
 SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "tarpit324@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python package for summarizing text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/" + AUTHOR_USER_NAME + "/" + SRC_REPO,
+    project_urls={
+        "Bug Tracker": "https://github.com/" + AUTHOR_USER_NAME + "/" + SRC_REPO + "/issues",
+
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
